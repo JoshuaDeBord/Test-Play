dd15cafc2b570d25ba4a76947b915462f56366cb
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-04-25 14:29:40 -0500

    Initial commit

---
+++ Notes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Play
+{
+    class Notes
+    {
+        /*
+            StopWatch
+            int stopWatch = 0;
+            stopWatch += 5;
+            Console.Clear();
+            Console.Write(stopWatch);
+            Thread.Sleep(1000);
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+namespace Test_Play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float timerMinuteInSeconds = 0.60f;
+            float timerTime = 0.0f;
+            float setTimer;
+
+
+
+            Console.WriteLine("Type \"timer\", \"stopwatch\", or \"clock\".");
+            
+            string clockChoice = Console.ReadLine();
+            
+            switch (clockChoice)
+            {
+                case "timer":
+                    Console.WriteLine("Type the amount of seconds you want your timer to have. Examples are");
+
+                    string timerChoice = Console.ReadLine();
+
+                   switch (timerChoice)
+                    {
+                        case ("0.30"):
+                            
+                            break;
+                       
+
+                        case ("1.00"):
+
+                            break;
+                    }
+
+
+
+                    break;
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
